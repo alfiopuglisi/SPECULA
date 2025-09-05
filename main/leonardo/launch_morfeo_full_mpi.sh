f8be812b6de81f957d84f83c1aa98d145b46d450
FabioRossiArcetri <rossi76@gmail.com>
2025-09-05 15:45:09 +0200

    Leonardo configs (#340)
    * (FRO) Fixing bugs in DataBiffer and AtmoRandomPhase.
    
    * (FRO) Bugs and tests fix
    
    * (FRO) Removing user specific folder from config files for launching on Leonardo.

--- main/leonardo/launch_morfeo_full_mpi.sh
+++ main/leonardo/launch_morfeo_full_mpi.sh
@@ -1,1 +1,1 @@
-python $WORK/alfio/SPECULA/main/scao/main_mpi.py $WORK/alfio/SPECULA/main/scao/params_morfeo_full.yml $WORK/alfio/SPECULA/main/scao/leonardo_mpi.yml
+python $WORK/SPECULA/main/scao/main_mpi.py $WORK/SPECULA/main/scao/params_morfeo_full.yml $WORK/SPECULA/main/scao/leonardo_mpi.yml
--- main/leonardo/launch_morfeo_full_mpi_distributed4.sh
+++ main/leonardo/launch_morfeo_full_mpi_distributed4.sh
@@ -1,1 +1,1 @@
-specula --mpi $WORK/alfio/SPECULA/main/scao/params_morfeo_full.yml $WORK/alfio/SPECULA/main/scao/leonardo_mpi_distributed4.yml
+specula --mpi $WORK/SPECULA/main/scao/params_morfeo_full.yml $WORK/SPECULA/main/scao/leonardo_mpi_distributed4.yml
